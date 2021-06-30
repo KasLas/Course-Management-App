56204a70c3d08d2a9e1e9c4a2a89e49a14ea0f5d
Kaspars Lasinskis <kasparsll@gmail.comm>
2021-06-30 15:15:14 +0300

    added App. HomePage, AboutPage and Rout

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>
+        This App uses React, Redux, React Router and many other useful libraries
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
