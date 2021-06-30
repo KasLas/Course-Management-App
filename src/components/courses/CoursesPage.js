f585a3b9f50300f5dafd49643589223a202eb713
Kaspars Lasinskis <kasparsll@gmail.comm>
2021-06-30 15:39:08 +0300

    Added CoursesPage to App and Header

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+// This is a stateful component fot demonstration porpoises
+
+class CoursesPage extends Component {
+  render() {
+    return <h2>Courses</h2>;
+  }
+}
+
+export default CoursesPage;
