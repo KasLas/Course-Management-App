20a2e2f85726c06609fba44ded078e36b80ffc66
Kaspars Lasinskis <kasparsll@gmail.comm>
2021-07-01 09:58:33 +0300

    added a basis for a form and submit

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -3,8 +3,36 @@ import React, { Component } from "react";
 // This is a stateful component fot demonstration porpoises
 
 class CoursesPage extends Component {
+  state = {
+    course: {
+      title: "",
+    },
+  };
+
+  handleChange = (event) => {
+    //   Copy state and set the title to input value
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course: course });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add a course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        ></input>
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
