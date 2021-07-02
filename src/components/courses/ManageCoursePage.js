8db326ffa283db100d42b99764128b7aba7c715a
Kaspars Lasinskis <kasparsll@gmail.comm>
2021-07-02 11:48:03 +0300

    converting ManageCoursePage to a functional component

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,32 +1,31 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 
-class ManageCoursePage extends Component {
-  componentDidMount() {
-    const { courses, authors, loadAuthors, loadCourses } = this.props;
-
+// Destructuring the props in the function signature
+function ManageCoursePage({ courses, authors, loadAuthors, loadCourses }) {
+  useEffect(() => {
     if (courses.length === 0) {
       loadCourses.loadCourses().catch((error) => {
         alert("Loading courses failed" + error);
       });
     }
+
     if (authors.length === 0) {
       loadAuthors.loadAuthors().catch((error) => {
         alert("Loading authors failed" + error);
       });
     }
-  }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
 }
 
 ManageCoursePage.propTypes = {
