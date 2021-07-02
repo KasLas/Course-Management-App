05e79a0e5f70380db5515ee74a3490597836f6e0
Kaspars Lasinskis <kasparsll@gmail.comm>
2021-07-02 09:50:49 +0300

    added authors to state

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
@@ -10,13 +11,24 @@ import CourseList from "./CourseList";
 class CoursesPage extends Component {
   static propTypes = {
     courses: PropTypes.array.isRequired,
+    authors: PropTypes.array.isRequired,
     actions: PropTypes.object.isRequired,
   };
 
   componentDidMount() {
-    this.props.actions.loadCourses().catch((error) => {
-      alert("Loading courses failed" + error);
-    });
+    // destructuring props for readability
+    const { courses, authors, actions } = this.props;
+
+    if (courses.length === 0) {
+      actions.loadCourses.loadCourses().catch((error) => {
+        alert("Loading courses failed" + error);
+      });
+    }
+    if (authors.length === 0) {
+      actions.loadAuthors.loadAuthors().catch((error) => {
+        alert("Loading authors failed" + error);
+      });
+    }
   }
 
   render() {
@@ -31,13 +43,28 @@ class CoursesPage extends Component {
 
 function mapStateToProps(state) {
   return {
-    courses: state.courses,
+    // this maps over courses array and maps the id field to corresponding author name aut of authors array!
+    // here is a safety check too state.authors.length === 0 ? [] : /return courses with authorName/ this re-renders the list component when authors are loaded
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map((course) => {
+            return {
+              ...course,
+              authorName: state.authors.find((a) => a.id === course.authorId)
+                .name,
+            };
+          }),
+    authors: state.authors,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(courseActions, dispatch),
+    actions: {
+      loadCourses: bindActionCreators(courseActions, dispatch),
+      loadAuthors: bindActionCreators(authorActions, dispatch),
+    },
   };
 }
 
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 // Root reducer - combines all reducers in to one
 import { combineReducers } from "redux";
 import courses from "./courseReducer";
+import authors from "./authorReducer";
 
 const rootReducer = combineReducers({
   courses: courses,
+  authors: authors,
 });
 
 export default rootReducer;
