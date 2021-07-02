da59fe0618454577782daa0082c1f31467d2a710
Kaspars Lasinskis <kasparsll@gmail.comm>
2021-07-02 08:57:31 +0300

    Added CourseList component

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
+import CourseList from "./CourseList";
 
 // This is a stateful component fot demonstration porpoises
 
@@ -22,10 +23,7 @@ class CoursesPage extends Component {
     return (
       <>
         <h2>Courses</h2>
-
-        {this.props.courses.map((course) => (
-          <div key={course.title}>{course.title}</div>
-        ))}
+        <CourseList courses={this.props.courses} />
       </>
     );
   }
