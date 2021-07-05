c1699d6dbe5f9934de014dfe96e070a128027519
Kaspars Lasinskis <kasparsll@gmail.comm>
2021-07-05 11:41:26 +0300

    added front end form validation

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -48,9 +48,26 @@ function ManageCoursePage({
     }));
   }
 
+  function formIsValid() {
+    const { title, authorId, category } = course;
+
+    const errors = {};
+
+    if (!title) errors.title = "Title is required!";
+    if (!authorId) errors.author = "Author is required!";
+    if (!category) errors.category = "Category is required!";
+
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  }
+
   function handleSave(event) {
     setIsSaving(true);
     event.preventDefault();
+    if (!formIsValid()) {
+      setIsSaving(false);
+      return;
+    }
     saveCourse(course)
       .then(() => {
         toast.success("Course saved.");
